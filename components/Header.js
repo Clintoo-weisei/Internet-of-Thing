fb44275e4c7320258697d787a6f84b735b9e58c5
Clintoo-weisei <clintonweisei03@gmail.com>
2025-03-29 21:47:26 +0300

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -24,7 +24,7 @@ function Header() {
         ];
 
         return (
-            <header data-name="header" className="header reflex top-0 z-50 shadow-md">
+            <header data-name="header" className="header reflex ">
             
                 <nav className="container mx-auto px-4 py-3">
                     <div className="flex justify-between items-center">
